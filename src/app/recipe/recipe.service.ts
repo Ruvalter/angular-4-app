077443579d35848fba2f6a449ba28b7b77ffb9c2
Mac Ruvalter <ruvalter@Macs-MacBook-Pro-66.local>
2017-07-18 21:05:48 -0400

    everything

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,13 +1,29 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model'
 
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is simply a test', 'http://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another recipe', 'This is simply a test', 'http://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+      'A test recipe',
+      'This is simply a test',
+      'http://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+        [
+          new Ingredient('Meat', 1),
+          new Ingredient('French Fries', 20)
+        ]),
+
+    new Recipe(
+      'Another recipe',
+      'This is simply a test',
+      'http://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+        [
+          new Ingredient('Bans', 2),
+          new Ingredient('Meat', 1)
+        ])
   ];
 
   getRecipes() {
